4cc73558db331264f0c1aec5492017d10cdbc5ea
mokuteki225 <kondratiuk011@gmail.com>
2024-11-16 04:03:57 +0200

    feat: decoding implementation

--- src/base64.zig
+++ src/base64.zig
@@ -48,8 +48,6 @@ const Base64 = struct {
             return output;
         }
 
-        std.debug.print("There are {d} hanging bytes\n", .{counter});
-
         const hanging_chunk = self.encodeChunk(input_buffer[0..counter]);
 
         output[output_index] = hanging_chunk[0];
@@ -60,26 +58,54 @@ const Base64 = struct {
         return output;
     }
 
+    pub fn decode(self: Base64, allocator: std.mem.Allocator, input: []const u8) ![]u8 {
+        var output_index: usize = 0;
+        const output_length = try calculateDecodeLength(input);
+        var output = try allocator.alloc(u8, output_length);
+
+        for (input, 0..) |_, i| {
+            if (i % 4 != 0) {
+                continue;
+            }
+
+            const left_boundary = i;
+            const right_boundary = if (left_boundary + 4 > input.len) input.len else left_boundary + 4;
+            const chunk = self.decodeChunk(input[left_boundary..right_boundary]);
+
+            for (0..3) |output_factor| {
+                if (output_index + output_factor >= output_length) {
+                    return output;
+                }
+
+                output[output_index + output_factor] = chunk[output_factor];
+            }
+
+            output_index += 3;
+        }
+
+        return output;
+    }
+
     fn encodeChunk(self: Base64, input: []const u8) [4]u8 {
         var output = [4]u8{ 0, 0, 0, 0 };
 
         if (input.len == 3) {
-            output[0] = self.at(input[0] >> 2);
-            output[1] = self.at(((input[0] & 0b00000011) << 4) ^ (input[1] >> 4));
-            output[2] = self.at(((input[1] & 0b00001111) << 2) ^ (input[2] >> 6));
-            output[3] = self.at(input[2] & 0b00111111);
+            output[0] = self.charAt(input[0] >> 2);
+            output[1] = self.charAt(((input[0] & 0b00000011) << 4) ^ (input[1] >> 4));
+            output[2] = self.charAt(((input[1] & 0b00001111) << 2) ^ (input[2] >> 6));
+            output[3] = self.charAt(input[2] & 0b00111111);
         }
 
         if (input.len == 2) {
-            output[0] = self.at(input[0] >> 2);
-            output[1] = self.at(((input[0] & 0b00000011) << 4) ^ (input[1] >> 4));
-            output[2] = self.at(((input[1] & 0b00001111) << 2));
+            output[0] = self.charAt(input[0] >> 2);
+            output[1] = self.charAt(((input[0] & 0b00000011) << 4) ^ (input[1] >> 4));
+            output[2] = self.charAt(((input[1] & 0b00001111) << 2));
             output[3] = '=';
         }
 
         if (input.len == 1) {
-            output[0] = self.at(input[0] >> 2);
-            output[1] = self.at(((input[0] & 0b00000011) << 4));
+            output[0] = self.charAt(input[0] >> 2);
+            output[1] = self.charAt(((input[0] & 0b00000011) << 4));
             output[2] = '=';
             output[3] = '=';
         }
@@ -87,8 +113,38 @@ const Base64 = struct {
         return output;
     }
 
-    fn at(self: Base64, index: u8) u8 {
+    fn decodeChunk(self: Base64, input: []const u8) [3]u8 {
+        var output = [3]u8{ 0, 0, 0 };
+
+        output[0] = (self.indexAt(input[0]) << 2) ^ (self.indexAt(input[1]) >> 4);
+
+        if (self.indexAt(input[2]) != 64) {
+            output[1] = (self.indexAt(input[1]) << 4) ^ (self.indexAt(input[2]) >> 2);
+        }
+
+        if (self.indexAt(input[3]) != 64) {
+            output[2] = (self.indexAt(input[2]) << 6) ^ self.indexAt(input[3]);
+        }
+
+        return output;
+    }
+
+    fn charAt(self: Base64, index: u8) u8 {
         return self._table[index];
+    }
+
+    fn indexAt(self: Base64, char: u8) u8 {
+        if (char == '=') {
+            return 64;
+        }
+
+        for (self._table, 0..) |element, index| {
+            if (element == char) {
+                return @intCast(index);
+            }
+        }
+
+        return 0;
     }
 };
 
@@ -109,10 +165,20 @@ fn calculateDecodeLength(input: []const u8) !usize {
         length = try std.math.divFloor(usize, input.len, 4);
     }
 
-    return length * 3;
+    length *= 3;
+
+    if (input[input.len - 1] == '=') {
+        length -= 1;
+    }
+
+    if (input.len > 1 and input[input.len - 2] == '=') {
+        length -= 1;
+    }
+
+    return length;
 }
 
-test "0 hanging bytes" {
+test "encode - 0 hanging bytes" {
     const base64 = Base64.init();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -125,7 +191,7 @@ test "0 hanging bytes" {
     try expect(std.mem.eql(u8, encoded, expected) == true);
 }
 
-test "2 hanging bytes" {
+test "encode - 2 hanging bytes" {
     const base64 = Base64.init();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -138,7 +204,7 @@ test "2 hanging bytes" {
     try expect(std.mem.eql(u8, encoded, expected) == true);
 }
 
-test "1 hanging bytes" {
+test "encode - 1 hanging byte" {
     const base64 = Base64.init();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -149,4 +215,43 @@ test "1 hanging bytes" {
     const expected = "YmFua2FpaQ==";
 
     try expect(std.mem.eql(u8, encoded, expected) == true);
+}
+
+test "decode - 1 hanging byte" {
+    const base64 = Base64.init();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const decoded: []u8 = try base64.decode(allocator, "YmFua2FpaQ==");
+    defer allocator.free(decoded);
+    const expected = "bankaii";
+
+    try expect(std.mem.eql(u8, decoded, expected) == true);
+}
+
+test "decode - 2 hanging bytes" {
+    const base64 = Base64.init();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const decoded: []u8 = try base64.decode(allocator, "TWFyaWE=");
+    defer allocator.free(decoded);
+    const expected = "Maria";
+
+    try expect(std.mem.eql(u8, decoded, expected) == true);
+}
+
+test "decode - 0 hanging bytes" {
+    const base64 = Base64.init();
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    const decoded: []u8 = try base64.decode(allocator, "SGVsbG8gd29ybGQh");
+    defer allocator.free(decoded);
+    const expected = "Hello world!";
+
+    try expect(std.mem.eql(u8, decoded, expected) == true);
 }
